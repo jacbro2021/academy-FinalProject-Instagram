d9d3df6319416b7f1eddf3afc7cba3cb209d8063
Jacob Brown <jsbrown@unc.edu>
2023-04-16 23:30:59 -0400

    added camera roll functionality for add post view.

--- Instagram/Instagram/Views/AddPostView.swift
+++ Instagram/Instagram/Views/AddPostView.swift
@@ -8,8 +8,59 @@
 import SwiftUI
 
 struct AddPostView: View {
+    
+    @State var caption = ""
+    @State var changePostImage = false
+    @State var openCameraRoll = false
+    @State var imageSelected = UIImage()
+    
     var body: some View {
-        Text("Add Post View")
+        VStack {
+            HStack {
+                Spacer()
+                Text("New Post")
+                    .font(.title3)
+                    .fontWeight(.medium)
+                    .padding(.leading, 50)
+                Spacer()
+                Button {
+                    //add post functionality here
+                } label: {
+                    Text("Post")
+                        .padding(.trailing)
+                        .font(.headline)
+                }
+            }
+            HStack {
+                Button {
+                    changePostImage = true
+                    openCameraRoll = true
+                } label: {
+                    if changePostImage == true {
+                        Image(uiImage: imageSelected)
+                            .resizable()
+                            .frame(width: 100, height: 100)
+                            .scaledToFill()
+                    } else {
+                        VStack {
+                            Image(systemName: "camera.fill")
+                                .foregroundColor(.primary)
+                                .font(.title)
+                            Text("add Image")
+                                .foregroundColor(.primary)
+                                .font(.caption)
+                        }
+                    }
+                }
+        
+                TextField("Write a caption...", text: $caption, axis: .vertical)
+                    .frame(width: 250, height: 100, alignment: .topLeading)
+                    .padding()
+            }
+            Spacer()
+        }.sheet(isPresented: $openCameraRoll) {
+            ImagePicker(selectedImage: $imageSelected,sourceType: .photoLibrary)
+        }
     }
 }
 
